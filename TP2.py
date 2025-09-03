b518d0147c145b3f9bee70018525023ce359dfd4
forgetl <forgetl@educ.college-montreal.qc.ca>
2025-09-03 14:54:53 -0400

    TP2

---
+++ TP2.py
@@ -0,0 +1,21 @@
+import random
+
+nb_aleatoire = random.randint(1, 100)
+
+debut = input("bonjour, desirez vous commencer le jeu?")
+
+if debut=oui():
+    nb_choisi = input("choisissez un nombre de 1 a 100")
+
+    if nb_choisi < 100: print("ceci n est pas une option")
+
+    if nb_choisi > 100: print("ceci n est pas une option")
+
+    if nb_choisi <= nb_aleatoire: print("meilleure chance la prochaine fois")
+
+    if nb_choisi => nb_aleatoire: print("meilleure chance la prochaine fois")
+
+    if nb_choisi = nb_aleatoire: print("bravo! desirez vous jouez une autre partie?")
+
+else():
+    print("au revoir")
